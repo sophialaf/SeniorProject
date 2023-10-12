605be280f9a378208aaf905c6d9abc5240671200
sophialaf <102073652+sophialaf@users.noreply.github.com>
2023-10-12 10:18:56 +0200

    added comments

--- proj/routes.js
+++ proj/routes.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+// Route for the home page
 router.get('/', (req, res) => {
     res.sendFile(__dirname + '/home.html');
 });
 
+// Route for the about us page
 router.get('/about', (req, res) => {
     res.sendFile(__dirname + '/aboutUs.html');
 });
 
+// Route for the events page
 router.get('/events', (req, res) => {
     res.sendFile(__dirname + '/events.html');
 });
 
+// Routes for various project pages
 router.get('/projects/bluebs', (req, res) => {
     res.sendFile(__dirname + '/project/blueb.html');
 });
@@ -21,38 +25,39 @@ router.get('/projects/avg', (req, res) => {
     res.sendFile(__dirname + '/project/AVG.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/bees', (req, res) => {
     res.sendFile(__dirname + '/project/bees.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/frontlines', (req, res) => {
     res.sendFile(__dirname + '/project/frontlines.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/lamaChopda', (req, res) => {
     res.sendFile(__dirname + '/project/lamaChopda.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/micro', (req, res) => {
     res.sendFile(__dirname + '/project/micro.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/ppp', (req, res) => {
     res.sendFile(__dirname + '/project/ppp.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/seva', (req, res) => {
     res.sendFile(__dirname + '/project/seva.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/uftp', (req, res) => {
     res.sendFile(__dirname + '/project/uftp.html');
 });
 
-router.get('/projects/avg', (req, res) => {
+router.get('/projects/wheelsUp', (req, res) => {
     res.sendFile(__dirname + '/project/wheelsUp.html');
 });
 
+// Route for the contact page
 router.get('/contact', (req, res) => {
     res.sendFile(__dirname + '/contact.html');
 });
