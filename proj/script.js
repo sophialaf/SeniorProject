57399096200cf8e1554479c386252d89a3328b78
sophialaf <102073652+sophialaf@users.noreply.github.com>
2023-10-12 10:48:15 +0200

    add jquery

--- proj/script.js
+++ proj/script.js
@@ -1,198 +1,56 @@
+const $ = require('jquery');
+
 document.addEventListener('DOMContentLoaded', function() {
-    /*-----------------------------------------
-    Navigation Event Listeners
-    -----------------------------------------*/
-
-    // Add an event listener to each navigation link
-    const navLinks = document.querySelectorAll('#topnav a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const page = event.target.getAttribute('href'); // Get the target route
-
-            // Update content based on the selected route
-            getContent(page);
-
-            // Add logic to update the active navigation link if needed
-            // ...
-        });
-    });
-
-    function getContent(page) {
-        /*-----------------------------------------
-        Fetch Content for Selected Page
-        -----------------------------------------*/
-
-        // Fetch the content for the selected page
-        fetch(page)
-            .then(response => response.text())
-            .then(data => {
-                document.querySelector('#content').innerHTML = data;
+    document.addEventListener('DOMContentLoaded', function() {
+        const navLinks = document.querySelectorAll('#topnav a');
+    
+        navLinks.forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                const page = event.target.getAttribute('href');
+                getContent(page);
             });
-    }
-
-    /*-----------------------------------------
-    Image Slideshow Functionality
-    -----------------------------------------*/
-
-    var images = document.querySelectorAll('.slideshow img');
-    var currentIndex = 0;
-    var intervalTime = 3000; // 3 seconds
-
-    setInterval(function () {
-        images[currentIndex].classList.remove('active');
-        currentIndex = (currentIndex + 1) % images.length;
-        images[currentIndex].classList.add('active');
-    }, intervalTime);
-
-    // Add your other script code here...
-});
-
-
-/*-----------------------------------------
-Form Display and Submission Handling
------------------------------------------*/
-
-var images = document.querySelectorAll('.slideshow img');
-var currentIndex = 0;
-var intervalTime = 3000; // 2 seconds
-
-document.getElementById('gib').addEventListener('click', function(event) {
-    event.preventDefault();
-
-    document.getElementById('formContainer').style.display = 'block';
-});
-
-document.getElementById('getInvolvedForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    console.log(formData);
-    fetch('http://localhost:3001/submitForm', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);
-        alert('Thank you for your interest!');
-        document.getElementById('formContainer').style.display = 'none';
-        document.getElementById('getInvolvedBtn').style.display = 'block';
-    })
-    .catch(error => console.error('Error:', error));
-});
-
-
-$(document).ready(function () {
-    /*-----------------------------------------
-    Image Slideshow Functionality (using jQuery)
-    -----------------------------------------*/
-
-    var slides = $('.slideshow img');
-    var currentSlide = 0;
-
-    function showSlide(index) {
-        slides.removeClass('active');
-        slides.eq(index).addClass('active');
-    }
-
-    function nextSlide() {
-        currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
-    }
-
-    setInterval(nextSlide, 3000);
-});
-
-
-$(document).ready(function () {
-    /*-----------------------------------------
-    Sticky Navigation
-    -----------------------------------------*/
-
-    var stickyOffset = $('#topnav').offset().top;
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() >= stickyOffset) {
-            $('#topnav').addClass('sticky');
-            $('#content').css('margin-top', $('#topnav').height() + 'px');
-        } else {
-            $('#topnav').removeClass('sticky');
-            $('#content').css('margin-top', '0');
+        });
+    
+        function getContent(page) {
+            fetch(page)
+                .then(response => response.text())
+                .then(data => {
+                    document.querySelector('#content').innerHTML = data;
+                });
         }
+    
+        var slides = $('.slideshow img');
+        var currentIndex = 0;
+    
+        function showSlide(index) {
+            slides.removeClass('active');
+            slides.eq(index).addClass('active');
+        }
+    
+        function nextSlide() {
+            currentIndex = (currentIndex + 1) % slides.length;
+            showSlide(currentIndex);
+        }
+    
+        setInterval(nextSlide, 3000);
+    
+        var stickyOffset = $('#topnav').offset().top;
+    
+        $(window).scroll(function () {
+            if ($(window).scrollTop() >= stickyOffset) {
+                $('#topnav').addClass('sticky');
+                $('#content').css('margin-top', $('#topnav').height() + 'px');
+            } else {
+                $('#topnav').removeClass('sticky');
+                $('#content').css('margin-top', '0');
+            }
+        });
+    
+        setInterval(function () {
+            images[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % images.length;
+            images[currentIndex].classList.add('active');
+        }, intervalTime);
     });
-});
-
-
-setInterval(function () {
-    /*-----------------------------------------
-    Image Slideshow Functionality
-    -----------------------------------------*/
-
-    images[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % images.length;
-    images[currentIndex].classList.add('active');
-}, intervalTime);
-
-
-function fillContent(url, elem) {
-    /*-----------------------------------------
-    Content Filling Logic
-    -----------------------------------------*/
-
-    document.querySelector("#one").className = "notCurrent";//Changes menu all to black
-    document.querySelector("#two").className = "notCurrent";
-    document.querySelector("#three").className = "notCurrent";
-
-    elem.className = "current";//Sets the current page to red highlight
-
-    fetch(url)//fetches html for content for each page
-        .then(function (response) { return response.text(); })
-        .then(function (data) {
-            document.querySelector('#content').innerHTML = data;
-        });
-}
-
-/*---------------sturman code--------------------------------------------------*/
-const listItems = document.querySelectorAll("#topnav li");
-for (i=0; i<listItems.length; i++) {
-    listItems[i].addEventListener("click", select);
-}
-getContent("home");
-
-function select(event) {
-    /*-----------------------------------------
-    Navigation Item Selection Handling
-    -----------------------------------------*/
-
-    const item = event.target;
-    getContent(event.taret.id);
-}
-
-function getContent(page) {
-    /*-----------------------------------------
-    Fetch Content for Selected Page (sturman code)
-    -----------------------------------------*/
-
-    fetch(page+".html")
-        .then(response => response.text()) // we are expecting a text response
-        .then(data => {
-            document.querySelector('#content').innerHTML = data;
-            setMenuItem(page);
-        });
-}
-
-function setMenuItem(selectedItem) {
-    /*-----------------------------------------
-    Set Active Menu Item
-    -----------------------------------------*/
-
-    for (i=0; i<listItems.length; i++) {
-        if (listItems[i].id == selectedItem) {
-            listItems[i].classList.add("item-selected");
-        } else {
-            listItems[i].classList.remove("item-selected");
-        }
-    }
-}
+})
--- proj/server.js
+++ proj/server.js
@@ -1,79 +1,45 @@
-/*------------------------------------------------------------------------------------------------------
-Initializing Express and Setting Up Middleware
-------------------------------------------------------------------------------------------------------*/
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// Importing necessary modules
-const express = require('express'); // Imports the Express framework
-const app = express(); // Creates an instance of the Express application
-const sqlite3 = require('sqlite3'); // Imports SQLite3 for database functionality
-const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+// Serve static files (images, CSS, etc.)
+app.use(express.static(path.join(__dirname, 'proj')));
 
-// Creating a connection to the database
-const db = new sqlite3.Database('myDatabase.db');
-
-/*------------------------------------------------------------------------------------------------------
-Database Connection Setup
-------------------------------------------------------------------------------------------------------*/
-
-// Middleware to parse JSON and urlencoded form data
-app.use(bodyParser.json()); // Parses incoming JSON requests
-app.use(bodyParser.urlencoded({ extended: true })); // Parses incoming URL-encoded form data
-
-// Creating a table for users if it doesn't already exist
-db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    firstName TEXT,
-    lastName TEXT,
-    projects TEXT,
-    age TEXT,
-    email TEXT,
-    phoneNumber TEXT
-    )
-    `, (err) => {
-        if (err) {
-            console.error('Error creating users table:', err);
-        } else {
-            console.log('Users table created successfully');
-        }
-    });
-
-/*------------------------------------------------------------------------------------------------------
-Handling Form Submission
-------------------------------------------------------------------------------------------------------*/
-
-// Define a route to handle form submission
-app.post('/submitForm', (req, res) => {
-    console.log(req.body); // Logs the data received from the form submission
-    const userData = req.body; // Extracts user data from the request body
-    db.run(
-        'INSERT INTO users (firstName, lastName, projects, age, email, phoneNumber) VALUES (?, ?, ?, ?, ?, ?)',
-        [userData.firstName, userData.lastName, userData.projects, userData.age, userData.email, userData.phoneNumber],
-        (err) => {
-            if (err) {
-                res.status(500).send(err.message); // Sends an error response if insertion fails
-            } else {
-                res.status(200).send('Data saved successfully!'); // Sends a success response if insertion is successful
-            }
-        }
-    );
+// Define routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'proj/Home.html'));
 });
 
-/*------------------------------------------------------------------------------------------------------
-Serving Static Files and HTML
-------------------------------------------------------------------------------------------------------*/
-
-app.use(express.static(__dirname)); // This line serves all files in the current directory
-
-// Serve the same HTML file for all routes
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/home.html'); // Sends the 'home.html' file as a response for all routes
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'proj/aboutUs.html'));
 });
 
-/*------------------------------------------------------------------------------------------------------
-Starting the Server
-------------------------------------------------------------------------------------------------------*/
+// Define routes for individual projects
+app.get('/projects/:projectName', (req, res) => {
+    const projectName = req.params.projectName;
+    const projectPath = path.join(__dirname, `proj/projects/${projectName}.html`);
 
-app.listen(3001, () => {
-    console.log('Server is running on http://localhost:3001'); // Starts the server and logs a message on successful startup
+    res.sendFile(projectPath);
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'proj/Contact.html'));
+});
+
+app.get('/events', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Events.html'));
+});
+
+// Define routes for shared elements (header, footer, etc.)
+app.get('/shared/:elementName', (req, res) => {
+    const elementName = req.params.elementName;
+    const elementPath = path.join(__dirname, `proj/shared/${elementName}.html`);
+
+    res.sendFile(elementPath);
+});
+
+// Start the server
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
