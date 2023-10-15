6978de6a75b82ba01a675d465344814225dd6888
sophialaf <102073652+sophialaf@users.noreply.github.com>
2023-10-15 11:56:53 +0200

    form buttons work again

--- proj2/script.js
+++ proj2/script.js
@@ -1,68 +1,54 @@
-function toggleFormVisibility() {
-    var formContainer = document.getElementById("formContainer");
-    var thankYouMessage = document.getElementById("thankYouMessage");
+// document.addEventListener('DOMContentLoaded', function () {
+//     var getInvolvedButton = document.getElementById('gib');
 
-    if (formContainer.style.display === "none" || formContainer.style.display === "") {
-        formContainer.style.display = "none";
-        thankYouMessage.style.display = "block";
-    } else {
-        formContainer.style.display = "block";
-        thankYouMessage.style.display = "none";
-    }
-}
+//     document.getElementById('getInvolvedForm').addEventListener('submit', function (event) {
+//         event.preventDefault();
+
+//         const formData = new FormData(this);
+
+//         fetch('/submitForm', {
+//             method: 'POST',
+//             body: formData
+//         })
+//             .then(response => response.text())
+//             .then(data => {
+//                 console.log(data); // Optional: Log the response from the server
+//             })
+//             .catch(error => {
+//                 console.error('Error:', error);
+//             });
+//     });
+// });
 
 document.addEventListener('DOMContentLoaded', function () {
     var getInvolvedButton = document.getElementById('gib');
+    var submitButton = document.getElementById('submitButton');
+    var formContainer = document.getElementById('formContainer');
 
     getInvolvedButton.addEventListener('click', function () {
-        toggleFormVisibility();
+        formContainer.style.display = 'block';
     });
 
-    document.getElementById('getInvolvedForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        const formData = new FormData(this);
-
-        fetch('/submitForm', {
-            method: 'POST',
-            body: formData
-        })
-            .then(response => response.text())
-            .then(data => {
-                console.log(data); // Optional: Log the response from the server
-                toggleFormVisibility(); // Close the form
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
+    submitButton.addEventListener('click', function () {
+        formContainer.style.display = 'none';
     });
 });
-    var slides = document.querySelectorAll('.slideshow img');
-    var currentIndex = 0;
 
-    function showSlide(index) {
-        slides.forEach(function (slide) {
-            slide.classList.remove('active');
-        });
-        slides[index].classList.add('active');
-    }
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        showSlide(currentIndex);
-    }
-
-    setInterval(nextSlide, 3000);
-
-    var stickyOffset = document.getElementById('topnav').offsetTop;
-
-    window.addEventListener('scroll', function () {
-        if (window.pageYOffset >= stickyOffset) {
-            document.getElementById('topnav').classList.add('sticky');
-            document.getElementById('content').style.marginTop = document.getElementById('topnav').offsetHeight + 'px';
-        } else {
-            document.getElementById('topnav').classList.remove('sticky');
-            document.getElementById('content').style.marginTop = '0';
-        }
+function fillContent(url, elem) {
+    // Remove 'current' class from all links
+    document.querySelectorAll('#topnav a').forEach(link => {
+        link.classList.remove('current');
+        link.classList.add('notCurrent');
     });
 
+    // Add 'current' class to the clicked link
+    elem.classList.remove('notCurrent');
+    elem.classList.add('current');
+
+    // Fetch and display content
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('#content').innerHTML = data;
+        });
+}
